d92ef8c956b9ef266984af524d4fc37c4d8cb605
Taynara Cruz <taynara.cruz@tcsindustrial.com.br>
2019-11-11 07:55:24 -0300

    Nova feature: Statement em HTML

--- src/Customer.java
+++ src/Customer.java
@@ -15,8 +15,28 @@ class Customer {
       return _name;
    };
 
+   private double getTotalCharge() {
+      double result = 0;
+      Enumeration<Rental> rentals = _rentals.elements();
+      while (rentals.hasMoreElements()) {
+         Rental each = (Rental) rentals.nextElement();
+         result += each.getCharge();
+      }
+      return result;
+   }
+
+   private int getTotalFrequentRenterPoints(){
+      int result = 0;
+      Enumeration<Rental> rentals = _rentals.elements();
+      while (rentals.hasMoreElements()) {
+         Rental each = (Rental) rentals.nextElement();
+         result += each.getFrequentRenterPoints();
+      }
+      return result;
+   }
+
    public String statement() {
-      Enumeration rentals = _rentals.elements();
+      Enumeration<Rental> rentals = _rentals.elements();
       String result = "Rental Record for " + getName() + "\n";
       while (rentals.hasMoreElements()) {
          Rental each = (Rental) rentals.nextElement();
@@ -32,23 +52,22 @@ class Customer {
                       " frequent renter points";
       return result;
    }
-   private double getTotalCharge() {
-      double result = 0;
+
+   public String htmlStatement() {
       Enumeration<Rental> rentals = _rentals.elements();
+      String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
       while (rentals.hasMoreElements()) {
          Rental each = (Rental) rentals.nextElement();
-         result += each.getCharge();
+         // show figures for each rental
+         result += each.getMovie().getTitle()+ ": " +
+                     String.valueOf(each.getCharge()) + "<BR>\n";
       }
+      
+      // add footer lines
+      result +=  "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+      result += "On this rental you earned <EM>" +
+               String.valueOf(getTotalFrequentRenterPoints()) +
+               "</EM> frequent renter points<P>";
       return result;
-    }
-
-    private int getTotalFrequentRenterPoints(){
-       int result = 0;
-       Enumeration<Rental> rentals = _rentals.elements();
-       while (rentals.hasMoreElements()) {
-          Rental each = (Rental) rentals.nextElement();
-          result += each.getFrequentRenterPoints();
-       }
-       return result;
-    }
+   }
 }
