2c2ffc03d0c5cdcbbc25439beddcf20ce0f996df
Taynara Cruz <taynara.cruz@tcsindustrial.com.br>
2019-11-11 07:43:00 -0300

    Refactoring 2: Rename

--- src/Customer.java
+++ src/Customer.java
@@ -15,26 +15,25 @@ class Customer {
       return _name;
    };
 
-  public double amountFor(Rental each){
-   double thisAmount = 0;
-   
-   switch (each.getMovie().getPriceCode()) {
-      case Movie.REGULAR:
-         thisAmount += 2;
-         if (each.getDaysRented() > 2)
-            thisAmount += (each.getDaysRented() - 2) * 1.5;
-         break;
-      case Movie.NEW_RELEASE:
-         thisAmount += each.getDaysRented() * 3;
-         break;
-      case Movie.CHILDRENS:
-         thisAmount += 1.5;
-         if (each.getDaysRented() > 3)
-            thisAmount += (each.getDaysRented() - 3) * 1.5;
-          break;
+   private double amountFor(Rental aRental) {
+      double result = 0;
+      switch (aRental.getMovie().getPriceCode()) {
+         case Movie.REGULAR:
+            result += 2;
+            if (aRental.getDaysRented() > 2)
+               result += (aRental.getDaysRented() - 2) * 1.5;
+            break;
+         case Movie.NEW_RELEASE:
+            result += aRental.getDaysRented() * 3;
+            break;
+         case Movie.CHILDRENS:
+            result += 1.5;
+            if (aRental.getDaysRented() > 3)
+               result += (aRental.getDaysRented() - 3) * 1.5;
+            break;
+      }
+      return result;
    }
-   return thisAmount;
-  }
   public String statement() {
    double totalAmount = 0;
    int frequentRenterPoints = 0;
